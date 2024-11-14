7d3d257d0c50870a6711bb27801ca8e716d7d9b0
Thiago Landi <thiagolandi0902@gmail.com>
2024-11-14 13:33:40 -0300

    findById implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,5 +1,12 @@
 package model.dao;
 
+import db.DB;
+import model.dao.impl.SellerDaoJDBC;
+
 public class DaoFactory {
 
+	public static SellerDao createSellerdao() { // isso permite que a implementação não fique exposta
+		return new SellerDaoJDBC(DB.getConnection());
+	}
+	
 }
